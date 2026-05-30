3f58a7b4e3bed49dcd3574619a83bef1f445e13b
Arjun <arjuarjun047@gmail.com>
2026-05-30 22:06:18 +0530

    Configure PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,15 +8,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 // Register Service Worker
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("/service-worker.js")
-//       .then((registration) => {
-//         console.log("SW registered:", registration);
-//       })
-//       .catch((error) => {
-//         console.log("SW registration failed:", error);
-//       });
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("SW registered:", registration);
+      })
+      .catch((error) => {
+        console.log("SW registration failed:", error);
+      });
+  });
+}
